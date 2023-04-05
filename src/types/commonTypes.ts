3dbf7ba1a15bb4d971936087c5a5d3c044f15872
uchenick55 <uchenick55@yandex.ru>
2023-04-05 22:20:05 +0300

    Updates

--- src/types/commonTypes.ts
+++
@@ -1,9 +0,0 @@
-export type cellType = {
-    id: string,
-    isBomb: boolean, // это бомба?
-    bombsClose: number, // количество бомб рядом
-    isShowed: boolean, // отображаются ли данные ячейки на поле
-    isBombFlag: boolean,
-}
-
-export type MainFieldType = Array<Array<cellType>>
